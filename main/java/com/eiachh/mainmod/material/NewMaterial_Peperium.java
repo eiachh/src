37c70da1653f4fa25bdec06e63f640d78f102f5f
batixsz <batixsz@gmail.com>
2018-12-28 20:06:06 +0100

    comment

--- main/java/com/eiachh/mainmod/material/NewMaterial_Peperium.java
+++ main/java/com/eiachh/mainmod/material/NewMaterial_Peperium.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class NewMaterial_Peperium {
-	//                                                                                      block                                          textures/models.armor <---"this"_layer_1.png armor png je
+	//                                                                                      block                               textures/models.armor <---"this"_layer_1.png armor png je       (Boots, Legs, Chest, Helm) half shield value
 	public static final ArmorMaterial TUTORIAL_ARMOR = EnumHelper.addArmorMaterial(MainModpls.MODID + ":" + "peperium", MainModpls.MODID + ":this", 16, new int[]{2, 5, 6, 3}, 5, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 10);
 
 }
